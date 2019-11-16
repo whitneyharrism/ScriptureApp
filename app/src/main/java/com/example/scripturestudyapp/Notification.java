b4176e5ca6b8797b24194e94526c930b3f1c52d2
cody <lil15003@byui.edu>
2019-11-16 07:46:41 -0500

    started notifications. Errors still exist because I am starting to get the idea of how it works. I will probably need help.

--- app/src/main/java/com/example/scripturestudyapp/Notification.java
+++ app/src/main/java/com/example/scripturestudyapp/Notification.java
@@ -1,14 +1,25 @@
 package com.example.scripturestudyapp;
 
+import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.util.Calendar;
+
+import static android.content.Context.ALARM_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
 
 //https://medium.com/exploring-android/exploring-android-o-notification-channels-94cd274f604c
 //https://developer.android.com/training/notify-user/build-notification
+//https://www.youtube.com/watch?v=1fV9NmvxXJo
 public class Notification {
+    String title;
     String message;
     String notificationSetting;
     String channelId = "some_channel_id";
@@ -20,23 +31,32 @@ public class Notification {
 
 
 
-    public void sendNotification(String message){
+    public void sendNotification(NotificationCompat.Builder builder){
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(channelId, builder.build());
     }
     public void setNotification(String message){
-
+        NotificationManagerCompat.notify(channelId, builder.build())
     }
-    public void createNotification(){
-//    NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-//            .setSmallIcon(R.drawable.notification_icon)
-//            .setContentTitle(textTitle)
-//            .setContentText(textContent)
-//            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+    public void createNotification(Context context){
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(res.drawable.iconBell)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(importance);
     }
     public void setChannel(String channelId, CharSequence channelName, int importance)
     {
         //NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, importance);
     }
-    public void setTime(int hour, int min){}
-    public void remove(){}
+    public void setTime(int hour, int min, Context context){
+        Calendar calendar = Calendar.getInstance();
+        Intent intent = new Intent(context, Notification.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,100,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+    }
+    public void remove(){cancel(channelName, channelId);}
 }
