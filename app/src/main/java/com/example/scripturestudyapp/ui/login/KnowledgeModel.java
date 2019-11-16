43e66d36b06751a70336e46832ad2e49bd7acc20
cody <lil15003@byui.edu>
2019-11-15 21:29:29 -0500

    finishing empty functions: KnowledgeModel

---
+++ app/src/main/java/com/example/scripturestudyapp/ui/login/KnowledgeModel.java
@@ -0,0 +1,11 @@
+package com.example.scripturestudyapp.ui.login;
+
+public class KnowledgeModel {
+    int bomScore;
+    int otScore;
+    int ntScore;
+    int pgpScore;
+    int dcScore;
+
+    public void getLowest(){}
+}
