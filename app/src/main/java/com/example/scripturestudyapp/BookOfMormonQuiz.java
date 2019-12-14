7ea212f4fa8201672d91e2546baf0f2bfdc9d2bd
cody <lil15003@byui.edu>
2019-12-14 00:03:00 -0500

    fixing xml files: hard-coded, size. Also fixed broken login link.

--- app/src/main/java/com/example/scripturestudyapp/BookOfMormonQuiz.java
+++ app/src/main/java/com/example/scripturestudyapp/BookOfMormonQuiz.java
@@ -34,13 +34,20 @@ public class BookOfMormonQuiz extends FactQuiz {
         q8.setAnswerChoices("To pray in their hearts", "Not to worship false idols", "To pray humbly", "To make a temple", 3);
         this.addQuestion(q1,q2,q3,q4,q5,q6,q7,q8);
 
+        //TODO Log.e("ths",mDatabase.child("Quiz").child("BOMQuiz").child("question").);
+        questionNumber = Integer.parseInt(mDatabase.child("Quiz").child("BOMQuiz").child("question").getKey());
+        for(int i = 0;i < questionNumber;i++)
+        {
+            questions.remove();
+        }
         currentQuestion = questions.peek();
-        saveProgress();
+//        saveProgress();
+
     }
 
     @Override
     public void saveProgress() {
-        mDatabase.child("Quiz").child("BOMQuiz").child("question").setValue("something");
+        mDatabase.child("Quiz").child("BOMQuiz").child("question").setValue(questionNumber);
         Log.e("BOMQuiz","setting");
     }
 }
